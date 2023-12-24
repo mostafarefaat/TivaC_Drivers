f4496ca3c28c6fa6bacbe6cad4816db64d3ebc61
mostafa <mostafarefaat456@gmail.com>
2023-12-24 16:42:23 +0200

    MCU driver implementation

---
+++ MCU.c
@@ -0,0 +1,223 @@
+/*-----------INCLUDES-----------*/
+#include "Mcu.h"
+#include "math.h"
+/*-----------LOCAL MACROS-----------*/
+uint8_t INT_on = 0;
+uint8_t SYS_TICK_INT_SERVED = 0;
+/*-----------GLOBAL STATIC VARIABLES-----------*/
+static STR_RELOAD_TYPE reload_value = 0;
+static STR_CONTROL_TYPE clk_source = 0;
+static STR_CONTROL_TYPE interrupt_config = 0;
+/*-----------GLOBAL EXTERN VARIABLES-----------*/
+
+/*-----------APIs IMPLEMENTATION-----------*/
+
+void Sys_Timer_init(const Clk_user_Config *clk_config_Ptr)
+{
+	reload_value = clk_config_Ptr->STR_Load_Value;
+	
+	clk_source =  clk_config_Ptr->Clk_Souce;
+	
+	interrupt_config =  clk_config_Ptr->INT_Config;
+	
+	/*1.CONFIG STCTRL REGISTER FOR REQUIRED OPERATION*/
+	/*1.1 CLOCK SOURCE*/
+	if(clk_source == PIOSC)
+	{
+		Dio_Write_Pin(&(STCTRL), CLK_SRC_PIN, LOW);
+	}
+	else if(clk_source == SYSTEM_CLK)
+	{
+		Dio_Write_Pin(&(STCTRL), CLK_SRC_PIN, HIGH);
+	}
+	
+	/*2. INTERRUPT ENABLE OR DISABLE*/
+	if(interrupt_config == INTERRUPT_ON)
+	{
+		Dio_Write_Pin(&(STCTRL), INTEN_PIN, HIGH);
+		INT_on = 1 ;
+	}
+	else if(interrupt_config == INTERRUPT_OFF)
+	{
+		Dio_Write_Pin(&(STCTRL), INTEN_PIN, LOW);
+	}
+	/*3. DELAY FUNCTION*/
+	if(reload_value > SYS_TIMER_MAX_VALUE)
+	{
+	Sys_Timer_delay();
+	}
+	else 
+	{
+		/*1.SET RELOAD VALUE IN STRELOAD REGISTER*/				
+		Dio_Write_Port(&(STRELOAD), reload_value-1);
+	
+		/*2.RESET CURRENT VALUE IN STCURRENT REGISTER AND TO CLEAR COUNT FLAG*/
+		Dio_Write_Port(&(STCURRENT), CURRENT_VALUE_RESET);
+	
+		/*4. ENABLE THE SYS_TIMER*/
+		Dio_Write_Pin(&(STCTRL), EN_PIN, HIGH);
+	}
+}
+
+void Sys_Timer_delay_1sec(uint32_t ticks)
+{
+		//uint8_t counts = 0;
+		/*1.SET RELOAD VALUE IN STRELOAD REGISTER*/				
+		Dio_Write_Port(&(STRELOAD), ticks-1);
+	
+		/*2.RESET CURRENT VALUE IN STCURRENT REGISTER AND TO CLEAR COUNT FLAG*/
+		Dio_Write_Port(&(STCURRENT), CURRENT_VALUE_RESET);
+	
+		/*4. ENABLE THE SYS_TIMER*/
+		Dio_Write_Pin(&(STCTRL), EN_PIN, HIGH);
+		
+		while((Dio_Read_Pin(&(STCTRL),PIN16))==0)		
+		{
+			if(SYS_TICK_INT_SERVED)
+			{
+				SYS_TICK_INT_SERVED = 0;
+				break;
+			}
+		}
+}
+
+void Sys_Timer_delay(void)
+{
+		uint8_t counts = 0;
+		uint8_t i = 0;
+		counts = ceil(reload_value / SYS_TIMER_1sec_VALUE );
+		Dio_Write_Pin(&(STCTRL), INTEN_PIN, LOW);
+		
+		for(i = 0; i< counts; i++)
+		{
+			if((INT_on == 1) && (i == (counts-1)))
+			{
+				Dio_Write_Pin(&(STCTRL), INTEN_PIN, HIGH);
+			}
+			
+			Sys_Timer_delay_1sec(SYS_TIMER_1sec_VALUE);
+		}	
+}
+
+void Module_Clk_init(MODULE_TYPE module)
+{
+	switch(module)
+	{
+		case GPIOA: RCGCGPIO |= (1<<PORTA);
+		break;
+		case GPIOB: RCGCGPIO |= (1<<PORTB);
+		break;
+		case GPIOC: RCGCGPIO |= (1<<PORTC);
+		break;
+		case GPIOD: RCGCGPIO |= (1<<PORTD);
+		break;
+		case GPIOE: RCGCGPIO |= (1<<PORTE);
+		break;
+		case GPIOF: RCGCGPIO |= (1<<PORTF);
+		break;
+		/*TODO: COMPLETE THE MODULES*/
+	  case DMA:	
+		break;
+		case UART:	
+		break;
+		case I2C:
+		break;
+		case CAN:
+		break;
+		case ADC:
+		break;
+		case PWM:
+		break;
+		case WDT0: RCGCWD |= (1<<PIN0);
+		break;		
+		case WDT1: RCGCWD |= (1<<PIN1);
+		break;
+		case Timer_0_module_16_32: RCGCTIMER |= (1<<PIN0);
+		break;
+		case Timer_1_module_16_32: RCGCTIMER |= (1<<PIN1);
+		break;
+		case Timer_2_module_16_32: RCGCTIMER |= (1<<PIN2);
+		break;
+		case Timer_3_module_16_32: RCGCTIMER |= (1<<PIN3);
+		break;
+		case Timer_4_module_16_32: RCGCTIMER |= (1<<PIN4);
+		break;
+		case Timer_5_module_16_32: RCGCTIMER |= (1<<PIN5);
+		break;
+		case Timer_0_module_32_64: RCGCWTIMER |= (1<<PIN0);
+		break;
+		case Timer_1_module_32_64: RCGCWTIMER |= (1<<PIN1);
+		break;
+		case Timer_2_module_32_64: RCGCWTIMER |= (1<<PIN2);
+		break;
+		case Timer_3_module_32_64: RCGCWTIMER |= (1<<PIN3);
+		break;
+		case Timer_4_module_32_64: RCGCWTIMER |= (1<<PIN4);
+		break;
+		case Timer_5_module_32_64: RCGCWTIMER |= (1<<PIN5);
+		break;
+	}
+	
+}
+
+void Module_Clk_Deinit(MODULE_TYPE module)
+{
+	switch(module)
+	{
+		case GPIOA: RCGCGPIO &= ~(1U<<PORTA);
+		break;
+		case GPIOB: RCGCGPIO &= ~(1U<<PORTB);
+		break;
+		case GPIOC: RCGCGPIO &= ~(1U<<PORTC);
+		break;
+		case GPIOD: RCGCGPIO &= ~(1U<<PORTD);
+		break;
+		case GPIOE: RCGCGPIO &= ~(1U<<PORTE);
+		break;
+		case GPIOF: RCGCGPIO &= ~(1U<<PORTF);
+		break;
+		/*TODO: COMPLETE THE MODULES*/
+	  case DMA:	
+		break;
+		case UART:	
+		break;
+		case I2C:
+		break;
+		case CAN:
+		break;
+		case ADC:
+		break;
+		case PWM:
+		break;
+		case WDT0: RCGCWD &= ~(1U<<PIN0);
+		break;		
+		case WDT1: RCGCWD &= ~(1U<<PIN1);
+		break;
+		case Timer_0_module_16_32: RCGCTIMER &= ~(1<<PIN0);
+		break;
+		case Timer_1_module_16_32: RCGCTIMER &= ~(1<<PIN1);
+		break;
+		case Timer_2_module_16_32: RCGCTIMER &= ~(1<<PIN2);
+		break;
+		case Timer_3_module_16_32: RCGCTIMER &= ~(1<<PIN3);
+		break;
+		case Timer_4_module_16_32: RCGCTIMER &= ~(1<<PIN4);
+		break;
+		case Timer_5_module_16_32: RCGCTIMER &= ~(1<<PIN5);
+		break;
+		case Timer_0_module_32_64: RCGCWTIMER &= ~(1<<PIN0);
+		break;
+		case Timer_1_module_32_64: RCGCWTIMER &= ~(1<<PIN1);
+		break;
+		case Timer_2_module_32_64: RCGCWTIMER &= ~(1<<PIN2);
+		break;
+		case Timer_3_module_32_64: RCGCWTIMER &= ~(1<<PIN3);
+		break;
+		case Timer_4_module_32_64: RCGCWTIMER &= ~(1<<PIN4);
+		break;
+		case Timer_5_module_32_64: RCGCWTIMER &= ~(1<<PIN5);
+		break;
+		
+	}	
+	
+}
---
+++ MCU.h
@@ -0,0 +1,84 @@
+#ifndef  MCU_H_
+#define  MCU_H_
+/*-----------INCLUDES-----------*/
+#include "Dio.h"
+
+typedef uint32_t 		STR_RELOAD_TYPE;
+typedef uint32_t 		STR_CURRENT_TYPE;
+typedef uint8_t 		STR_CONTROL_TYPE;
+typedef uint8_t 		MODULE_TYPE;
+/*-----------LOCAL MACROS-----------*/
+
+#define BASE_ADDRESS					0XE000E000
+#define RELOAD_VALUE_MASK			0xFFFFFF
+#define CURRENT_VALUE_RESET		0X00000000
+#define SYS_TIMER_MAX_VALUE		16777215
+#define SYS_TIMER_1sec_VALUE	16000000
+#define	PIOSC									0
+#define	SYSTEM_CLK						1
+#define	INTERRUPT_ON					1
+#define	INTERRUPT_OFF					0
+
+
+/*-----------STCTRL PINS-----------*/
+#define EN_PIN						0
+#define INTEN_PIN					1
+#define CLK_SRC_PIN				2
+#define COUNT_PIN					16
+
+/*-----------SYSTEM TIMER REGISTERS-----------*/ 
+#define STCTRL 			 		*((volatile uint32_t *)(BASE_ADDRESS + 0x010))
+#define STRELOAD 			 	*((volatile uint32_t *)(BASE_ADDRESS + 0x014))
+#define STCURRENT 			*((volatile uint32_t *)(BASE_ADDRESS + 0x018))
+	
+/*-----------SYSTEM CONTROL REGISTERS-----------*/  /*PAGE 232*/
+
+#define RCGCGPIO 		*((volatile uint32_t *)0x400FE608)
+#define RCGCDMA 		*((volatile uint32_t *)0x400FE60C)
+#define RCGCUART 		*((volatile uint32_t *)0x400FE618)
+#define RCGCI2C 		*((volatile uint32_t *)0x400FE620)
+#define RCGCCAN 		*((volatile uint32_t *)0x400FE634)
+#define RCGCADC 		*((volatile uint32_t *)0x400FE638)
+#define RCGCPWM 		*((volatile uint32_t *)0x400FE640)
+#define RCGCWD			*((volatile uint32_t *)0x400FE600)
+#define RCGCTIMER			*((volatile uint32_t *)0x400FE604)	
+#define RCGCWTIMER			*((volatile uint32_t *)0x400FE65C)
+
+
+#define PRGPIO		 	*((volatile uint32_t *)( 0x400FEA08))
+
+/*-----------END OF MCU REGISTERS-----------*/
+
+
+/*-----------GLOBAL STATIC VARIABLES-----------*/
+
+ typedef struct
+ {
+	 STR_RELOAD_TYPE 		STR_Load_Value;
+	 STR_CONTROL_TYPE		Clk_Souce;
+	 STR_CONTROL_TYPE		INT_Config;
+	 
+	 
+ }Clk_user_Config;
+ 
+ enum Module
+ {
+	 GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF, DMA, UART, I2C, CAN, ADC, PWM, WDT0, WDT1,	/*PAGE 104*/
+	 Timer_0_module_16_32, Timer_1_module_16_32, Timer_2_module_16_32, Timer_3_module_16_32, Timer_4_module_16_32, Timer_5_module_16_32,
+	 Timer_0_module_32_64, Timer_1_module_32_64, Timer_2_module_32_64, Timer_3_module_32_64, Timer_4_module_32_64, Timer_5_module_32_64,
+	 
+ };
+	
+/*-----------GLOBAL EXTERN VARIABLES-----------*/
+ 
+
+/*-----------APIs FUNCTIONS DECLARATION-----------*/
+void Sys_Timer_init(const Clk_user_Config *clk_config_Ptr );
+void Sys_Timer_delay_1sec(uint32_t ticks );
+void Sys_Timer_delay(void);
+void Module_Clk_init(MODULE_TYPE module);
+void Module_Clk_Deinit(MODULE_TYPE module);
+
+//STR_RELOAD_TYPE reload_value_hex, STR_CONTROL_TYPE clk_source, STR_CONTROL_TYPE interrupt_config
+
+#endif
